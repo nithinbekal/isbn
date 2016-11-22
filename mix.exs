957538e0d042a7264ba5b93dfb85bfe4f5a9d091
Nithin Bekal <nithinbekal@gmail.com>
2016-11-22 11:57:20 +0530

    Add package information

--- mix.exs
+++ mix.exs
@@ -7,14 +7,22 @@ defmodule ISBN.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: [],
+     package: package,
+    ]
   end
 
   def application do
     [applications: [:logger]]
   end
 
-  defp deps do
-    []
+  defp package do
+    [
+      name: :isbn,
+      files: ["lib", "mix.exs"],
+      maintainers: ["Nithin Bekal"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/nithinbekal/isbn"},
+    ]
   end
 end
